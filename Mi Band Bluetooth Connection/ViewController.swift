adcab00038436730fa9fb55c6e1a3077e5ab2e68
TOGNI Elie <Elie@macbook-pro-de-togni.home>
2018-08-06 13:10:32 +0200

    Premier commit

--- Mi Band Bluetooth Connection/ViewController.swift
+++ Mi Band Bluetooth Connection/ViewController.swift
@@ -7,18 +7,55 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CBCentralManagerDelegate {
+    
+    var manager: CBCentralManager!
+    
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        manager = CBCentralManager(delegate: self, queue: nil)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        
+        print("Peripheral : \(peripheral)")
     }
+    
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        
+        var consoleMsg = ""
+        
+        switch (central.state) {
+        case .poweredOff:
+            consoleMsg = "BLE is powered off"
+        case .poweredOn:
+            consoleMsg = "BLE is powered on"
+            manager.scanForPeripherals(withServices: nil, options: nil)
+        case .resetting:
+            consoleMsg = "BLE is resetting"
+        case .unauthorized:
+            consoleMsg = "BLE is unauthorized"
+        case .unknown:
+            consoleMsg = "BLE is unknown"
+        case .unsupported:
+            consoleMsg = "BLE is unsupported"
+      
+        }
+        
+        print("\(consoleMsg)")
+        
+        
+        
+    }
+ 
 
 
 }
